e756d43c21e2d585464eaccf811778aa67b1de04
Egor Orlenok <messaf@mail.ru>
2023-08-06 23:36:49 +1000

    Update test work

--- main.py
+++ main.py
@@ -8,8 +8,5 @@ def run_app():
     print(f'Приложение заметки!')
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     run_app()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
